ba7bb3b46784bbe28b042b4550592bd35210a35a
AlexandrZayts <sanya9-1998@inbox.ru>
2022-12-08 22:43:46 +0300

    1

--- NewsPaper/NewsPortal/models.py
+++ NewsPaper/NewsPortal/models.py
@@ -2,8 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Author(models.Model):
-    Author_User = models.OneToOneField(User)
-    author_rating = models.IntegerField(default = 0)
+    Author_User = models.OneToOneField(User, on_delete=models.CASCADE)
+    author_rating = models.IntegerField(default=0)
 
     def update_rating(self):
         self.author_rating = Post.objects.filter(Post_Author=Author).values("post_rating") * 3 \
@@ -13,7 +13,7 @@ class Author(models.Model):
 
 
 class Category(models.Model):
-    category = models.CharField(unique = True)
+    category = models.CharField(max_length=64, unique=True)
 
 
 article = 'ar'
@@ -23,14 +23,15 @@ choic = [
     (news, 'новость')
 ]
 
+
 class Post(models.Model):
-    Post_Author = models.ForeignKey('Author', on_delete=models.CASCADE)
-    position = models.CharField(choices=choic, default=news)
+    Post_Author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    position = models.CharField(max_length=2 ,choices=choic, default=news)
     post_time = models.DateTimeField(auto_now_add=True)
-    Post_PostCategory = models.ManyToManyField(Category, through = 'PostCategory')
+    Post_PostCategory = models.ManyToManyField(Category, through='PostCategory')
     post_header = models.CharField(max_length=64, default="Без темы")
     post_text = models.TextField()
-    post_rating = models.IntegerField(default = 0)
+    post_rating = models.IntegerField(default=0)
 
     def like(self):
         return self.post_rating + 1
@@ -43,14 +44,15 @@ class Post(models.Model):
 
 
 class PostCategory(models.Model):
-    PostCategory_Post = models.ForeignKey('Post', on_delete=models.CASCADE)
-    PostCategory_Category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    PostCategory_Post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    PostCategory_Category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
 
 class Comment(models.Model):
-    Comment_Post = models.ForeignKey('Post', on_delete=models.CASCADE)
-    Comment_User = models.ForeignKey('User', on_delete=models.CASCADE)
-    comment_text = models.TextField(max_length = 255) # Из задания не ясно только авторизованные пользователи могут оставлять комментарии или нет, ограничения задано для исключения спама от гостей.
+    Comment_Post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    Comment_User = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment_text = models.TextField(
+        max_length=255)  # Из задания не ясно только авторизованные пользователи могут оставлять комментарии или нет, ограничения задано для исключения спама от гостей.
     comment_time = models.DateTimeField(auto_now_add=True)
     comment_rating = models.IntegerField(default=0)
 
